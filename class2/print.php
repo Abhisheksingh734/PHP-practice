fa2ff1078c186b234116ec710a89c9f6844e0ba9
Abhishek <abhisheksinghasr734@gmail.com>
2024-08-21 15:33:19 +0530

    class3 grade

---
+++ class2/print.php
@@ -0,0 +1,27 @@
+<?php
+
+$car = ["Zen", "Maruti", "Fiat"];
+
+for($i=0;$i<sizeof($car);$i++){
+    echo $car[$i];
+    echo "<br>";
+}
+
+
+$x = [1,5,3,8,5,7,3,9];
+
+
+$max = PHP_INT_MIN;
+$min = PHP_INT_MAX;
+
+for($i=0;$i<sizeof($x);$i++){
+    if($x[$i]>$max) $max = $x[$i];
+    if($x[$i]<$min) $min = $x[$i];
+}
+
+echo "Max element in array is ".$max."<br>";
+echo "Min element in array is ".$min;
+
+
+
+?>
---
+++ class3/gradeCalculator.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    
+      <form action="" method="post">
+        <input type="text" name="sub1" placeholder="Enter subject 1 marks">
+        <input type="text" name="sub2" placeholder="Enter subject 2 marks">
+        <input type="text" name="sub3" placeholder="Enter subject 3 marks">
+        <input type="text" name="sub4" placeholder="Enter subject 4 marks">
+        <input type="text" name="sub5" placeholder="Enter subject 5 marks">
+        <button type="submit" name="submit">Submit</button>
+      </form>
+
+      <?php
+        if(isset($_POST['submit'])){
+            $sub1 = $_POST['sub1'];
+            $sub2 = $_POST['sub2'];
+            $sub3 = $_POST['sub3'];
+            $sub4 = $_POST['sub4'];
+            $sub5 = $_POST['sub5'];
+            $total = $sub1 + $sub2 + $sub3 + $sub4 + $sub5;
+            $percentage = $total / 5; 
+            echo $percentage;  
+        }
+        ?>
+    
+
+ </body>
+ </html>
